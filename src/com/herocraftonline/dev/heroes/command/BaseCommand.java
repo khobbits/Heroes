30089298e4a5aff632d9f5756fd2274f5fdfb9b3
Rigby <rigby@onarandombox.com>
2011-06-10 00:06:01 +0100

    Oops, great commit message is GREAT.

--- src/com/herocraftonline/dev/heroes/command/BaseCommand.java
+++ src/com/herocraftonline/dev/heroes/command/BaseCommand.java
@@ -13,7 +13,7 @@ public abstract class BaseCommand {
     protected String name;
     protected String description;
     protected String usage;
-    protected String permissionNode;
+    protected String permissionNode = "";
     protected int minArgs;
     protected int maxArgs;
     protected List<String> identifiers;
