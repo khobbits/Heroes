de39b7ee8607b6085242907b799471d3ffbd8b48
DThielke <dave.thielke@gmail.com>
2011-07-05 14:01:59 -0400

    lol.

--- src/com/herocraftonline/dev/heroes/party/PartyEntityListener.java
+++ src/com/herocraftonline/dev/heroes/party/PartyEntityListener.java
@@ -9,27 +9,28 @@ import org.bukkit.event.entity.EntityListener;
 
 import com.herocraftonline.dev.heroes.Heroes;
 
-public class PartyEntityListener extends EntityListener{
+public class PartyEntityListener extends EntityListener {
     public Heroes plugin;
-    
+
     public PartyEntityListener(Heroes plugin) {
         this.plugin = plugin;
     }
-    
-    public void onEntity(EntityDamageEvent initialEvent) {
+
+    @Override
+    public void onEntityDamage(EntityDamageEvent initialEvent) {
         plugin.log(Level.INFO, initialEvent.getCause().toString());
-        if(initialEvent instanceof EntityDamageByEntityEvent) {
+        if (initialEvent instanceof EntityDamageByEntityEvent) {
             EntityDamageByEntityEvent subEvent = (EntityDamageByEntityEvent) initialEvent;
-            if(subEvent.getEntity() instanceof Player && subEvent.getDamager() instanceof Player) {
+            if (subEvent.getEntity() instanceof Player && subEvent.getDamager() instanceof Player) {
                 Player attacker = (Player) subEvent.getDamager();
                 Player defender = (Player) subEvent.getEntity();
                 HeroParty attackParty = plugin.getHeroManager().getHero(attacker).getParty();
-                
-                if(attackParty == null) {
+
+                if (attackParty == null) {
                     return;
                 }
                 plugin.log(Level.INFO, Boolean.toString(attackParty.isPartyMember(defender)));
-                if(attackParty.isPartyMember(defender) && attackParty.getPvp()) {
+                if (attackParty.isPartyMember(defender) && attackParty.getPvp()) {
                     initialEvent.setCancelled(true);
                 }
             }
