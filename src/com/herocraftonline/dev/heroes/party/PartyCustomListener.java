1a8aad85de2affbe6542e9b8e4d238a606966732
RightLegRed <nath-daws@hotmail.co.uk>
2011-07-06 00:23:36 +0100

    Fixing an NPE

--- src/com/herocraftonline/dev/heroes/party/PartyCustomListener.java
+++ src/com/herocraftonline/dev/heroes/party/PartyCustomListener.java
@@ -10,25 +10,30 @@ import com.herocraftonline.dev.heroes.persistence.Hero;
 
 public class PartyCustomListener extends CustomEventListener{
     Heroes plugin;
-    
+
     public PartyCustomListener(Heroes plugin) {
         this.plugin = plugin;
     }
-    
+
     public void onCustomEvent(Event event) {
         if(event instanceof ExperienceGainEvent) {
             ExperienceGainEvent subEvent = (ExperienceGainEvent) event;
-            if(!(subEvent.getHero().getParty() != null) && subEvent.getHero().getParty().getMembers().size() > 0 && subEvent.getHero().getParty().getExp()) {
+            if(subEvent.getHero().getParty() != null && subEvent.getHero().getParty().getExp()) {
                 return;
             }
+
+            if(subEvent.getHero().getParty().getMembers().size() > 0) {
+                return;
+            }
+            
             Hero hero = subEvent.getHero();
             Integer expGain = Math.round(subEvent.getExpGain() / hero.getParty().getMembers().size());
 
             for(Player p : hero.getParty().getMembers()) {
                 if(p != subEvent.getHero().getPlayer()) {
-                Hero pHero = plugin.getHeroManager().getHero(p);
-                pHero.gainExp(expGain, subEvent.getSource());
-            }
+                    Hero pHero = plugin.getHeroManager().getHero(p);
+                    pHero.gainExp(expGain, subEvent.getSource());
+                }
             }
             subEvent.setExpGain(expGain);
         }
