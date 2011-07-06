72906a8c499c6e0c1d9ec84ea9f675821b9515a3
RightLegRed <nath-daws@hotmail.co.uk>
2011-07-06 23:05:51 +0100

    Renamed to sharedExpGain and removed modifier

--- src/com/herocraftonline/dev/heroes/persistence/Hero.java
+++ src/com/herocraftonline/dev/heroes/persistence/Hero.java
@@ -204,10 +204,9 @@ public class Hero {
         }
     }
 
-    public void quietExpGain(int expGain, ExperienceType source) {
+    public void sharedExpGain(int expGain, ExperienceType source) {
         int exp = getExperience();
         // Work out the correct amount of Exp to award using the Classes Modifier.
-        expGain = (int) (expGain * this.getHeroClass().getExpModifier());
 
         Properties prop = plugin.getConfigManager().getProperties();
         int currentLevel = prop.getLevel(exp);
