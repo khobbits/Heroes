6b97d63409c83d82fb944fa495b19f99eafa5ca6
RightLegRed <nath-daws@hotmail.co.uk>
2011-07-11 15:36:17 +0100

    Port changed to teleport nearby party members

--- src/com/herocraftonline/dev/heroes/skill/skills/SkillPort.java
+++ src/com/herocraftonline/dev/heroes/skill/skills/SkillPort.java
@@ -1,6 +1,9 @@
 package com.herocraftonline.dev.heroes.skill.skills;
 
+import java.util.List;
+
 import org.bukkit.Location;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
 import com.herocraftonline.dev.heroes.Heroes;
@@ -12,7 +15,7 @@ public class SkillPort extends ActiveSkill {
     public SkillPort(Heroes plugin) {
         super(plugin);
         setName("Port");
-        setDescription("Teleports you to the set location!");
+        setDescription("Teleports you and your nearby party to the set location!");
         setUsage("/skill port <location>");
         setMinArgs(1);
         setMaxArgs(1);
@@ -24,6 +27,15 @@ public class SkillPort extends ActiveSkill {
         Player player = hero.getPlayer();
         if (getSetting(hero.getHeroClass(), args[0].toLowerCase(), null) != null) {
             String[] splitArg = getSetting(hero.getHeroClass(), args[0].toLowerCase(), null).split(":");
+            List<Entity> surrounding = player.getNearbyEntities(10, 10, 10);
+            for(Entity n : surrounding) {
+                if(n instanceof Player  ) {
+                    Player playerN = (Player)n;
+                    if(plugin.getHeroManager().getHero(playerN).getParty() == hero.getParty()){
+                        playerN.teleport(new Location(hero.getPlayer().getWorld(), Double.parseDouble(splitArg[0]), Double.parseDouble(splitArg[1]), Double.parseDouble(splitArg[2])));
+                    }
+                }
+            }
             player.teleport(new Location(hero.getPlayer().getWorld(), Double.parseDouble(splitArg[0]), Double.parseDouble(splitArg[1]), Double.parseDouble(splitArg[2])));
             notifyNearbyPlayers(player.getLocation(), getUseText(), player.getName(), getName());
             return true;
