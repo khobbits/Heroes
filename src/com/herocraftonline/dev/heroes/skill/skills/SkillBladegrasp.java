af8f65c84b57577338be22d589667eb9b3dba48c
Sleaker <sleaker@gmail.com>
2011-08-09 18:26:30 -0700

    Spam control for Bladegrasp

--- src/com/herocraftonline/dev/heroes/skill/skills/SkillBladegrasp.java
+++ src/com/herocraftonline/dev/heroes/skill/skills/SkillBladegrasp.java
@@ -80,9 +80,13 @@ public class SkillBladegrasp extends ActiveSkill {
     }
 
     public class SkillEntityListener extends EntityListener {
-
+        
         @Override
         public void onEntityDamage(EntityDamageEvent event) {
+            //Ignore cancelled damage events & 0 damage events for Spam Control
+            if (event.getDamage() == 0 || event.isCancelled())
+                return;
+            
             Entity defender = event.getEntity();
             if (defender instanceof Player) {
                 Player player = (Player) defender;
