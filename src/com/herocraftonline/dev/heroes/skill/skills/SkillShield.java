d01ede9f106dc9633a0eb06b6c0699dfe12986f4
Sleaker <sleaker@gmail.com>
2011-09-22 21:52:04 -0700

    Shield was listening for wrong door type.

--- src/com/herocraftonline/dev/heroes/skill/skills/SkillShield.java
+++ src/com/herocraftonline/dev/heroes/skill/skills/SkillShield.java
@@ -2,7 +2,6 @@ package com.herocraftonline.dev.heroes.skill.skills;
 
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.event.CustomEventListener;
 import org.bukkit.event.Event;
 import org.bukkit.event.Event.Priority;
 import org.bukkit.event.Event.Type;
@@ -10,6 +9,7 @@ import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.util.config.ConfigurationNode;
 
 import com.herocraftonline.dev.heroes.Heroes;
+import com.herocraftonline.dev.heroes.api.HeroesEventListener;
 import com.herocraftonline.dev.heroes.api.WeaponDamageEvent;
 import com.herocraftonline.dev.heroes.hero.Hero;
 import com.herocraftonline.dev.heroes.skill.PassiveSkill;
@@ -35,7 +35,29 @@ public class SkillShield extends PassiveSkill {
         return node;
     }
 
-    public class CustomListener extends CustomEventListener {
+    public class CustomListener extends HeroesEventListener {
+
+
+        @Override
+        public void onWeaponDamage(WeaponDamageEvent event) {
+            if (event.getCause() != DamageCause.ENTITY_ATTACK || event.getDamage() == 0 || !(event.getEntity() instanceof Player))
+                return;
+
+            Player player = (Player) event.getEntity();
+            Hero hero = plugin.getHeroManager().getHero(player);
+            if (hero.hasEffect(getName())) {
+                double multiplier = 1;
+                if (player.getItemInHand().getType() == Material.IRON_DOOR) {
+                    multiplier = getSetting(hero.getHeroClass(), "iron-door", 0.75);
+                } else if (player.getItemInHand().getType() == Material.WOOD_DOOR) {
+                    multiplier = getSetting(hero.getHeroClass(), "wooden-door", 0.85);
+                } else if (player.getItemInHand().getType() == Material.TRAP_DOOR) {
+                    multiplier = getSetting(hero.getHeroClass(), "trapdoor", 0.60);
+                }
+                event.setDamage((int) (event.getDamage() * multiplier));
+
+            }
+        }
 
         @Override
         public void onCustomEvent(Event event) {
@@ -43,23 +65,7 @@ public class SkillShield extends PassiveSkill {
                 return;
 
             WeaponDamageEvent subEvent = (WeaponDamageEvent) event;
-            if (subEvent.getCause() != DamageCause.ENTITY_ATTACK || subEvent.getDamage() == 0)
-                return;
-            if (subEvent.getEntity() instanceof Player) {
-                Player player = (Player) subEvent.getEntity();
-                Hero hero = plugin.getHeroManager().getHero(player);
-                if (hero.hasEffect(getName())) {
-                    double multiplier = 1;
-                    if (player.getItemInHand().getType() == Material.IRON_DOOR) {
-                        multiplier = getSetting(hero.getHeroClass(), "iron-door", 0.75);
-                    } else if (player.getItemInHand().getType() == Material.WOODEN_DOOR) {
-                        multiplier = getSetting(hero.getHeroClass(), "wooden-door", 0.85);
-                    } else if (player.getItemInHand().getType() == Material.TRAP_DOOR) {
-                        multiplier = getSetting(hero.getHeroClass(), "trapdoor", 0.60);
-                    }
-                    subEvent.setDamage((int) (subEvent.getDamage() * multiplier));
-                }
-            }
+
         }
     }
 }
