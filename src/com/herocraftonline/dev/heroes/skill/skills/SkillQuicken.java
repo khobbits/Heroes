33b9e76aae9e2d01af60efea8faa802425f814aa
Sleaker <sleaker@gmail.com>
2011-09-26 21:59:04 -0700

    added Quicken - out of combat longer duration sprint - is cancelled when the player takes damage

---
+++ src/com/herocraftonline/dev/heroes/skill/skills/SkillQuicken.java
@@ -0,0 +1,99 @@
+package com.herocraftonline.dev.heroes.skill.skills;
+
+import com.herocraftonline.dev.heroes.skill.SkillType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityListener;
+import org.bukkit.util.config.ConfigurationNode;
+
+import com.herocraftonline.dev.heroes.Heroes;
+import com.herocraftonline.dev.heroes.effects.EffectType;
+import com.herocraftonline.dev.heroes.effects.ExpirableEffect;
+import com.herocraftonline.dev.heroes.hero.Hero;
+import com.herocraftonline.dev.heroes.skill.ActiveSkill;
+import com.herocraftonline.dev.heroes.skill.Skill;
+import com.herocraftonline.dev.heroes.util.Setting;
+
+public class SkillQuicken extends ActiveSkill {
+
+    private String applyText;
+    private String expireText;
+
+    public SkillQuicken(Heroes plugin) {
+        super(plugin, "Quicken");
+        setDescription("Provides a movement speed boost until you get hit");
+        setUsage("/skill quicken");
+        setArgumentRange(0, 0);
+        setIdentifiers("skill quicken", "skill quick");
+        setTypes(SkillType.BUFF, SkillType.MOVEMENT, SkillType.SILENCABLE);
+    }
+
+    @Override
+    public ConfigurationNode getDefaultConfig() {
+        ConfigurationNode node = super.getDefaultConfig();
+        node.setProperty("speed-multiplier", 2);
+        node.setProperty(Setting.DURATION.node(), 300000);
+        node.setProperty("apply-text", "%hero% gained a burst of speed!");
+        node.setProperty("expire-text", "%hero% returned to normal speed!");
+        return node;
+    }
+
+    @Override
+    public void init() {
+        super.init();
+        applyText = getSetting(null, "apply-text", "%hero% gained a burst of speed!").replace("%hero%", "$1");
+        expireText = getSetting(null, "expire-text", "%hero% returned to normal speed!").replace("%hero%", "$1");
+    }
+
+    @Override
+    public boolean use(Hero hero, String[] args) {
+        broadcastExecuteText(hero);
+
+        int duration = getSetting(hero.getHeroClass(), Setting.DURATION.node(), 300000);
+        int multiplier = getSetting(hero.getHeroClass(), "speed-multiplier", 2);
+        if (multiplier > 20) {
+            multiplier = 20;
+        }
+        hero.addEffect(new QuickenEffect(this, duration, multiplier));
+
+        return true;
+    }
+
+    public class QuickenEffect extends ExpirableEffect {
+
+        public QuickenEffect(Skill skill, long duration, int amplifier) {
+            super(skill, "Quicken", duration);
+            this.types.add(EffectType.DISPELLABLE);
+            this.types.add(EffectType.BENEFICIAL);
+            addMobEffect(1, (int) (duration / 1000) * 20, amplifier);
+        }
+
+        @Override
+        public void apply(Hero hero) {
+            super.apply(hero);
+            Player player = hero.getPlayer();
+            broadcast(player.getLocation(), applyText, player.getDisplayName());
+        }
+
+        @Override
+        public void remove(Hero hero) {
+            super.remove(hero);
+            Player player = hero.getPlayer();
+            broadcast(player.getLocation(), expireText, player.getDisplayName());
+        }
+    }
+    
+    public class SkillEntityListener extends EntityListener {
+
+        @Override
+        public void onEntityDamage(EntityDamageEvent event) {
+            if (event.isCancelled() || event.getDamage() == 0 || !(event.getEntity() instanceof Player))
+                return;
+            
+            Hero hero = plugin.getHeroManager().getHero((Player) event.getEntity());
+            if (hero.hasEffect("Quicken"))
+                hero.removeEffect(hero.getEffect("Quicken"));
+        }
+        
+    }
+}
