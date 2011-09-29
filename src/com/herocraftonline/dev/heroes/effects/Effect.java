2049c3cb36c882985e78ceeeb9a61c479ba127b0
Sleaker <sleaker@gmail.com>
2011-09-28 17:31:33 -0700

    Effect hashCode doesn't need to be nearly this fancy. name should never be null, ever. - and multiplying identical hashes by identical numbers is a waste.

--- src/com/herocraftonline/dev/heroes/effects/Effect.java
+++ src/com/herocraftonline/dev/heroes/effects/Effect.java
@@ -137,10 +137,7 @@ public class Effect {
 
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + (name == null ? 0 : name.hashCode());
-        return result;
+        return name.hashCode();
     }
 
     /**
