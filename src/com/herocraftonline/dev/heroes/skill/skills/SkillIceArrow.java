94c1970840fb6f4fd574f458f32fa1cd18c0cb60
Sleaker <sleaker@gmail.com>
2011-10-21 16:00:56 -0700

    fix default message on icearrow

--- src/com/herocraftonline/dev/heroes/skill/skills/SkillIceArrow.java
+++ src/com/herocraftonline/dev/heroes/skill/skills/SkillIceArrow.java
@@ -44,7 +44,7 @@ public class SkillIceArrow extends ActiveSkill {
         node.setProperty("speed-multiplier", 2);
         node.setProperty(Setting.DURATION.node(), 60000); // milliseconds
         node.setProperty("attacks", 1); // How many attacks the buff lasts for.
-        node.setProperty(Setting.APPLY_TEXT.node(), "%target% imbue's their arrows with ice!");
+        node.setProperty(Setting.APPLY_TEXT.node(), "%target% imbues their arrows with ice!");
         node.setProperty(Setting.EXPIRE_TEXT.node(), "%target%'s arrows are no longer imbued with ice!");
         return node;
     }
