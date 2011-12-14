96da82240ab0337ce236d38c17efbd2e41194c45
sleak <sleak@sleak-PC>
2011-12-13 22:35:54 -0800

    revert sending message. We'd rather not spam the user.

--- src/com/herocraftonline/dev/heroes/skill/TargettedSkill.java
+++ src/com/herocraftonline/dev/heroes/skill/TargettedSkill.java
@@ -132,7 +132,7 @@ public abstract class TargettedSkill extends ActiveSkill {
         if (target == null) {
             // don't self-target harmful skills
             if (this.isType(SkillType.HARMFUL))
-                return SkillResult.INVALID_TARGET;
+                return SkillResult.INVALID_TARGET_NO_MSG;
             target = player;
         }
 
