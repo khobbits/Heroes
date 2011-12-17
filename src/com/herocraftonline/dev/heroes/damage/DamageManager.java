745fcc8391632527e34d4c023dc1b3c3b101d067
sleak <sleak@sleak-PC>
2011-12-16 23:45:28 -0800

    damage system should now work with WorldGuard fully

--- src/com/herocraftonline/dev/heroes/damage/DamageManager.java
+++ src/com/herocraftonline/dev/heroes/damage/DamageManager.java
@@ -176,7 +176,7 @@ public class DamageManager {
      */
     public void registerEvents() {
         PluginManager pluginManager = plugin.getServer().getPluginManager();
-        pluginManager.registerEvent(Type.ENTITY_DAMAGE, listener, Priority.High, plugin);
+        pluginManager.registerEvent(Type.ENTITY_DAMAGE, listener, Priority.Highest, plugin);
         pluginManager.registerEvent(Type.ENTITY_REGAIN_HEALTH, listener, Priority.Highest, plugin);
     }
 
