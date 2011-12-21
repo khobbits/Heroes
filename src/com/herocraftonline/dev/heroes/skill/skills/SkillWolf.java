90659de333aae406e4f9ce42181f20f9a4393d4d
sleak <sleak@sleak-PC>
2011-12-21 12:54:26 -0800

    castrated the wolf

--- src/com/herocraftonline/dev/heroes/skill/skills/SkillWolf.java
+++ src/com/herocraftonline/dev/heroes/skill/skills/SkillWolf.java
@@ -1,48 +1,21 @@
 package com.herocraftonline.dev.heroes.skill.skills;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
-
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.block.BlockFace;
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.AnimalTamer;
-import org.bukkit.entity.CreatureType;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Wolf;
 import org.bukkit.event.Event.Priority;
 import org.bukkit.event.Event.Type;
-import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityListener;
 import org.bukkit.event.entity.EntityTameEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerListener;
-import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.event.world.ChunkUnloadEvent;
-import org.bukkit.event.world.WorldListener;
 
 import com.herocraftonline.dev.heroes.Heroes;
-import com.herocraftonline.dev.heroes.api.ClassChangeEvent;
-import com.herocraftonline.dev.heroes.api.HeroesEventListener;
-import com.herocraftonline.dev.heroes.api.SkillResult;
-import com.herocraftonline.dev.heroes.api.WeaponDamageEvent;
 import com.herocraftonline.dev.heroes.hero.Hero;
-import com.herocraftonline.dev.heroes.skill.ActiveSkill;
-import com.herocraftonline.dev.heroes.skill.Skill;
-import com.herocraftonline.dev.heroes.skill.SkillConfigManager;
+import com.herocraftonline.dev.heroes.skill.PassiveSkill;
 import com.herocraftonline.dev.heroes.skill.SkillType;
 import com.herocraftonline.dev.heroes.util.Messaging;
-import com.herocraftonline.dev.heroes.util.Setting;
 
-public class SkillWolf extends ActiveSkill {
-
-    public boolean skillTaming = true;
-    public Set<Wolf> wolves = new HashSet<Wolf>();
+public class SkillWolf extends PassiveSkill {
 
     public SkillWolf(Heroes plugin) {
         super(plugin, "Wolf");
@@ -50,107 +23,11 @@ public class SkillWolf extends ActiveSkill {
         setUsage("/skill wolf <release|summon>");
         setArgumentRange(0, 1);
         setIdentifiers("skill wolf");
-        setTypes(SkillType.SUMMON);
+        setTypes(SkillType.SUMMON, SkillType.KNOWLEDGE);
 
-        SkillEntityListener seListener = new SkillEntityListener(this);
-        SkillPlayerListener spListener = new SkillPlayerListener(this);
-
-        registerEvent(Type.CHUNK_UNLOAD, new SkillChunkListener(), Priority.Highest);
-        registerEvent(Type.ENTITY_TAME, seListener, Priority.Highest);
-        registerEvent(Type.ENTITY_DEATH, seListener, Priority.Monitor);
-        registerEvent(Type.PLAYER_JOIN, spListener, Priority.High);
-        registerEvent(Type.PLAYER_QUIT, spListener, Priority.Lowest);
-        registerEvent(Type.CUSTOM_EVENT, new SkillHeroListener(this), Priority.Highest);
+        registerEvent(Type.ENTITY_TAME, new SkillEntityListener(this), Priority.Highest);
     }
 
-    @Override
-    public ConfigurationSection getDefaultConfig() {
-        ConfigurationSection node = super.getDefaultConfig();
-        node.set(Setting.MAX_DISTANCE.node(), 5);
-        node.set("max-wolves", 3);
-        node.set(Setting.DAMAGE.node(), 3);
-        node.set("damage-per-level", .1);
-        node.set("tame-requires-skill", true);
-        return node;
-    }
-
-    @Override
-    public void init() {
-        super.init();
-        skillTaming = SkillConfigManager.getRaw(this, "tame-requires-skill", true);
-    }
-
-    @Override
-    public SkillResult use(Hero hero, String[] args) {
-        Player player = hero.getPlayer();
-
-        if (args.length == 0) {
-
-            int wolves = 0;
-            if (hero.getSkillSettings(this) != null) {
-                wolves = Integer.parseInt(hero.getSkillSettings(this).get("wolves"));
-            }
-
-            int maxWolves = SkillConfigManager.getUseSetting(hero, this, "max-wolves", 3, false);
-            if (wolves >= maxWolves) {
-                Messaging.send(player, "You already have the maximum number of summons.");
-                return SkillResult.FAIL;
-            }
-
-            int distance = SkillConfigManager.getUseSetting(hero, this, Setting.MAX_DISTANCE, 5, false);
-            Location castLoc = player.getTargetBlock((HashSet<Byte>) null, distance).getLocation();
-            if (castLoc.getBlock().getType() != Material.AIR) {
-                castLoc = castLoc.getBlock().getRelative(BlockFace.UP).getLocation();
-            }
-
-            if (castLoc.getBlock().getType() != Material.AIR) {
-                Messaging.send(player, "No room to summon a wolf at that location!");
-                return SkillResult.FAIL;
-            }
-
-            Wolf wolf = (Wolf) player.getWorld().spawnCreature(castLoc, CreatureType.WOLF);
-            setWolfSettings(hero, wolf);
-            hero.setSkillSetting(this, "wolves", wolves + 1);
-            broadcastExecuteText(hero);
-            return SkillResult.NORMAL;
-        } else if (args[0].equals("summon")) {
-            boolean summoned = false;
-            for (LivingEntity lEntity : hero.getSummons()) {
-                if (lEntity instanceof Wolf) {
-                    summoned = true;
-                    lEntity.teleport(player);
-                }
-            }
-            if (!summoned) {
-                Messaging.send(player, "You have no wolves to summon.");
-            } else {
-                broadcast(player.getLocation(), "$1 has summoned wolves to their side!", player.getDisplayName());
-                return SkillResult.NORMAL;
-            }
-        } else if (args[0].equals("release")) {
-            Iterator<LivingEntity> iter = hero.getSummons().iterator();
-            while (iter.hasNext()) {
-                LivingEntity lEntity = iter.next();
-                if (lEntity instanceof Wolf) {
-                    iter.remove();
-                    lEntity.remove();
-                }
-            }
-
-            hero.setSkillSetting(this, "wolves", 0);
-            broadcast(player.getLocation(), "$1 has released their wolves into the wild", player.getDisplayName());
-        }
-
-        return SkillResult.SKIP_POST_USAGE;
-    }
-
-    private void setWolfSettings(Hero hero, Wolf wolf) {
-        Player player = hero.getPlayer();
-        wolf.setOwner(player);
-        wolf.setTamed(true);
-        hero.getSummons().add(wolf);
-        wolves.add(wolf);
-    }
 
     public class SkillEntityListener extends EntityListener {
 
@@ -158,34 +35,6 @@ public class SkillWolf extends ActiveSkill {
 
         SkillEntityListener(SkillWolf skill) {
             this.skill = skill;
-        }
-
-        @Override
-        public void onEntityDeath(EntityDeathEvent event) {
-            Heroes.debug.startTask("HeroesSkillListener.Wolf");
-            if (!(event.getEntity() instanceof Wolf)) {
-                Heroes.debug.stopTask("HeroesSkillListener.Wolf");
-                return;
-            }
-
-            Wolf wolf = (Wolf) event.getEntity();
-            AnimalTamer owner = wolf.getOwner();
-            if (!wolf.isTamed() || owner == null || !(owner instanceof Player)) {
-                Heroes.debug.stopTask("HeroesSkillListener.Wolf");
-                return;
-            }
-
-            Hero hero = plugin.getHeroManager().getHero((Player) owner);
-            if (!hero.getSummons().contains(wolf)) {
-                Heroes.debug.stopTask("HeroesSkillListener.Wolf");
-                return;
-            }
-
-            hero.getSummons().remove(wolf);
-            wolves.remove(wolf);
-            int wolves = Integer.parseInt(hero.getSkillSettings(skill).get("wolves"));
-            hero.setSkillSetting(skill, "wolves", wolves - 1);
-            Heroes.debug.stopTask("HeroesSkillListener.Wolf");
         }
 
         @Override
@@ -200,164 +49,9 @@ public class SkillWolf extends ActiveSkill {
 
             Player player = (Player) owner;
             Hero hero = plugin.getHeroManager().getHero(player);
-            int numWolves = 0;
-            for (LivingEntity lEntity : hero.getSummons()) {
-                if (lEntity instanceof Wolf) {
-                    numWolves++;
-                }
-            }
-            if (skill.skillTaming && !hero.canUseSkill(skill.getName())) {
+            if (!hero.canUseSkill(skill.getName())) {
+                Messaging.send(player, "You can't tame wolves!");
                 event.setCancelled(true);
-            } else {
-                if (numWolves >= SkillConfigManager.getUseSetting(hero, skill, "max-wolves", 3, false)) {
-                    event.setCancelled(true);
-                    Messaging.send(player, "You can't tame anymore wolves!");
-                    return;
-                }
-                skill.setWolfSettings(hero, (Wolf) animal);
-                Messaging.send(player, "You have tamed a wolf!");
-            }
-            Heroes.debug.stopTask("HeroesSkillListener.Wolf");
-        }
-    }
-
-    public class SkillHeroListener extends HeroesEventListener {
-
-        private Skill skill;
-
-        public SkillHeroListener(Skill skill) {
-            this.skill = skill;
-        }
-
-        @Override
-        public void onClassChange(ClassChangeEvent event) {
-            Heroes.debug.startTask("HeroesSkillListener.Wolf");
-            if (event.isCancelled()) {
-                Heroes.debug.stopTask("HeroesSkillListener.Wolf");
-                return;
-            }
-
-            Hero hero = event.getHero();
-
-            Iterator<LivingEntity> iter = hero.getSummons().iterator();
-            while (iter.hasNext()) {
-                LivingEntity lEntity = iter.next();
-                if (lEntity instanceof Wolf) {
-                    lEntity.remove();
-                    wolves.remove(lEntity);
-                    iter.remove();
-                }
-            }
-            Map<String, String> skillSettings = hero.getSkillSettings(skill);
-            if (skillSettings != null) {
-                hero.setSkillSetting(skill, "wolves", 0);
-            }
-            Heroes.debug.stopTask("HeroesSkillListener.Wolf");
-        }
-
-        @Override
-        public void onWeaponDamage(WeaponDamageEvent event) {
-            Heroes.debug.startTask("HeroesSkillListener.Wolf");
-            if (!(event.getDamager() instanceof Wolf) || event.isCancelled()) {
-                Heroes.debug.stopTask("HeroesSkillListener.Wolf");
-                return;
-            }
-
-            Wolf wolf = (Wolf) event.getDamager();
-            AnimalTamer owner = wolf.getOwner();
-            if (!wolf.isTamed() || owner == null || !(owner instanceof Player)) {
-                Heroes.debug.stopTask("HeroesSkillListener.Wolf");
-                return;
-            }
-
-            Hero hero = plugin.getHeroManager().getHero((Player) owner);
-            if (!hero.getSummons().contains(wolf)) {
-                Heroes.debug.stopTask("HeroesSkillListener.Wolf");
-                return;
-            }
-
-            double damagePerLevel = SkillConfigManager.getUseSetting(hero, skill, "damage-per-level", .1, false);
-            int damage = SkillConfigManager.getUseSetting(hero, skill, Setting.DAMAGE, 3, false) + (int) (hero.getSkillLevel(skill) * damagePerLevel);
-            event.setDamage(damage);
-            Heroes.debug.stopTask("HeroesSkillListener.Wolf");
-        }
-    }
-
-    public class SkillChunkListener extends WorldListener {
-
-        @Override
-        public void onChunkUnload(ChunkUnloadEvent event) {
-            Heroes.debug.startTask("HeroesSkillListener.Wolf");
-            if (event.isCancelled()) {
-                Heroes.debug.stopTask("HeroesSkillListener.Wolf");
-                return;
-            }
-
-            Set<Wolf> wolvesClone = new HashSet<Wolf>(wolves);
-            for (Wolf wolf : wolvesClone) {
-                Location loc = wolf.getLocation();
-                if (event.getChunk().getX() == (loc.getBlockX() >> 4) && (event.getChunk().getZ() == loc.getBlockZ() >> 4)) {
-                    Player owner = (Player) wolf.getOwner();
-                    if (owner.isOnline()) {
-                        Wolf newWolf = (Wolf) owner.getWorld().spawnCreature(owner.getLocation(), CreatureType.WOLF);
-                        setWolfSettings(plugin.getHeroManager().getHero(owner), newWolf);
-                        wolves.add(newWolf);
-                    }
-                    wolves.remove(wolf);
-                    wolf.remove();
-                }
-            }
-            Heroes.debug.stopTask("HeroesSkillListener.Wolf");
-        }
-    }
-
-    public class SkillPlayerListener extends PlayerListener {
-
-        private final Skill skill;
-
-        public SkillPlayerListener(Skill skill) {
-            this.skill = skill;
-        }
-
-        @Override
-        public void onPlayerJoin(PlayerJoinEvent event) {
-            Heroes.debug.startTask("HeroesSkillListener.Wolf");
-            Player player = event.getPlayer();
-            Hero hero = plugin.getHeroManager().getHero(player);
-
-            if (!hero.canUseSkill(skill) || hero.getSkillSettings(skill) == null) {
-                Heroes.debug.stopTask("HeroesSkillListener.Wolf");
-                return;
-            }
-
-            if (hero.getSkillSettings(skill).containsKey("wolves")) {
-                int wolves = Integer.parseInt(hero.getSkillSettings(skill).get("wolves"));
-                for (int i = 0; i < wolves; i++) {
-                    Wolf wolf = (Wolf) player.getWorld().spawnCreature(player.getLocation(), CreatureType.WOLF);
-                    setWolfSettings(hero, wolf);
-                }
-            }
-            Heroes.debug.stopTask("HeroesSkillListener.Wolf");
-        }
-
-        @Override
-        public void onPlayerQuit(PlayerQuitEvent event) {
-            Heroes.debug.startTask("HeroesSkillListener.Wolf");
-            Hero hero = plugin.getHeroManager().getHero(event.getPlayer());
-            if (!hero.canUseSkill("Wolf")) {
-                Heroes.debug.stopTask("HeroesSkillListener.Wolf");
-                return;
-            }
-
-            Iterator<LivingEntity> iter = hero.getSummons().iterator();
-            while (iter.hasNext()) {
-                LivingEntity lEntity = iter.next();
-                if (lEntity instanceof Wolf) {
-                    System.out.println("removing wolf");
-                    lEntity.remove();
-                    wolves.remove(lEntity);
-                    iter.remove();
-                }
             }
             Heroes.debug.stopTask("HeroesSkillListener.Wolf");
         }
