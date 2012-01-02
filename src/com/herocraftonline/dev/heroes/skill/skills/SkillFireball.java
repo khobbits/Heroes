d1faf4e2222d12c251f857d8b5760712e40ecc3e
Sleaker <sleaker@comcast.net>
2012-01-02 09:45:01 -0800

    reverted fireball to use snowballs until smallfireball is better.

--- src/com/herocraftonline/dev/heroes/skill/skills/SkillFireball.java
+++ src/com/herocraftonline/dev/heroes/skill/skills/SkillFireball.java
@@ -1,18 +1,10 @@
 package com.herocraftonline.dev.heroes.skill.skills;
 
-import net.minecraft.server.EntityLiving;
-import net.minecraft.server.EntitySmallFireball;
-import net.minecraft.server.Vec3D;
-
-import org.bukkit.Location;
-import org.bukkit.block.Block;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.craftbukkit.CraftWorld;
-import org.bukkit.craftbukkit.entity.CraftPlayer;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.SmallFireball;
+import org.bukkit.entity.Snowball;
 import org.bukkit.event.Event.Priority;
 import org.bukkit.event.Event.Type;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -28,7 +20,6 @@ import com.herocraftonline.dev.heroes.skill.Skill;
 import com.herocraftonline.dev.heroes.skill.SkillConfigManager;
 import com.herocraftonline.dev.heroes.skill.SkillType;
 import com.herocraftonline.dev.heroes.util.Setting;
-import com.herocraftonline.dev.heroes.util.Util;
 
 public class SkillFireball extends ActiveSkill {
 
@@ -38,7 +29,6 @@ public class SkillFireball extends ActiveSkill {
         setUsage("/skill fireball");
         setArgumentRange(0, 0);
         setIdentifiers("skill fireball");
-
         setTypes(SkillType.FIRE, SkillType.SILENCABLE, SkillType.DAMAGING, SkillType.HARMFUL);
 
         registerEvent(Type.ENTITY_DAMAGE, new SkillEntityListener(this), Priority.Normal);
@@ -49,42 +39,16 @@ public class SkillFireball extends ActiveSkill {
         ConfigurationSection node = super.getDefaultConfig();
         node.set(Setting.DAMAGE.node(), 4);
         node.set("fire-ticks", 100);
-        node.set(Setting.DEATH_TEXT.node(), "%target% was burned alive by %hero%'s fireball!");
         return node;
     }
 
     @Override
     public SkillResult use(Hero hero, String[] args) {
         Player player = hero.getPlayer();
-        Block target = player.getTargetBlock(Util.transparentIds, 100);
-        Location playerLoc = player.getLocation();
-
-        double dx = target.getX() - playerLoc.getX();
-        double height = 1;
-        double dy = target.getY() + (height / 2.0F) - (playerLoc.getY() + (height / 2.0F));
-        double dz = target.getZ() - playerLoc.getZ();
-
-        EntityLiving playerEntity = ((CraftPlayer) player).getHandle();
-        EntitySmallFireball fireball = new EntitySmallFireball(((CraftWorld) player.getWorld()).getHandle(), playerEntity, dx, dy, dz);
-        fireball.isIncendiary = false;
-        Vec3D vec3d = Util.getLocation(player);
-        fireball.locX = playerLoc.getX() + vec3d.a + .5D;
-        fireball.locY = playerLoc.getY() + (height / 2.0F) + 0.5D;
-        fireball.locZ = playerLoc.getZ() + vec3d.c + .5D;
-
-        ((CraftWorld) player.getWorld()).getHandle().addEntity(fireball);
-
-        broadcastExecuteText(hero);
-        return SkillResult.NORMAL;
-        /*
-        Player player = hero.getPlayer();
-
         Snowball snowball = player.throwSnowball();
         snowball.setFireTicks(1000);
-
         broadcastExecuteText(hero);
         return SkillResult.NORMAL;
-        */
     }
 
     public class SkillEntityListener extends EntityListener {
@@ -102,16 +66,16 @@ public class SkillFireball extends ActiveSkill {
                 Heroes.debug.stopTask("HeroesSkillListener");
                 return;
             }
-            
+
             EntityDamageByEntityEvent subEvent = (EntityDamageByEntityEvent) event;
             Entity projectile = subEvent.getDamager();
-            if (!(projectile instanceof SmallFireball)) {
+            if (!(projectile instanceof Snowball) || projectile.getFireTicks() < 1) {
                 Heroes.debug.stopTask("HeroesSkillListener");
                 return;
             }
 
             LivingEntity entity = (LivingEntity) subEvent.getEntity();
-            Entity dmger = ((SmallFireball) projectile).getShooter();
+            Entity dmger = ((Snowball) projectile).getShooter();
             if (dmger instanceof Player) {
                 Hero hero = plugin.getHeroManager().getHero((Player) dmger);
 
@@ -127,7 +91,7 @@ public class SkillFireball extends ActiveSkill {
                     plugin.getHeroManager().getHero((Player) entity).addEffect(new CombustEffect(skill, (Player) dmger));
                 } else
                     plugin.getEffectManager().addEntityEffect(entity, new CombustEffect(skill, (Player) dmger));
-                
+
                 addSpellTarget(entity, hero);
                 int damage = SkillConfigManager.getUseSetting(hero, skill, Setting.DAMAGE, 4, false);
                 event.setDamage(damage);
