f2efc13cca1349d85198996b4d8cdbd5563ddea4
Sleaker <sleaker@comcast.net>
2012-01-22 23:18:01 -0800

    brewing stand/enchant table blocking fix.

--- src/com/herocraftonline/dev/heroes/skill/skills/SkillAlchemy.java
+++ src/com/herocraftonline/dev/heroes/skill/skills/SkillAlchemy.java
@@ -2,23 +2,21 @@ package com.herocraftonline.dev.heroes.skill.skills;
 
 import java.util.logging.Level;
 
-import net.minecraft.server.ContainerBrewingStand;
-import net.minecraft.server.EntityPlayer;
-
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.craftbukkit.entity.CraftPlayer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.getspout.spoutapi.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 import com.herocraftonline.dev.heroes.Heroes;
 import com.herocraftonline.dev.heroes.effects.EffectType;
 import com.herocraftonline.dev.heroes.hero.Hero;
 import com.herocraftonline.dev.heroes.skill.PassiveSkill;
-import com.herocraftonline.dev.heroes.skill.Skill;
 import com.herocraftonline.dev.heroes.skill.SkillType;
+import com.herocraftonline.dev.heroes.util.Messaging;
 import com.herocraftonline.dev.heroes.util.Setting;
 
 public class SkillAlchemy extends PassiveSkill {
@@ -29,39 +27,31 @@ public class SkillAlchemy extends PassiveSkill {
         setArgumentRange(0, 0);
         setTypes(SkillType.KNOWLEDGE, SkillType.ITEM);
         setEffectTypes(EffectType.BENEFICIAL);
-        
+
         if (Heroes.useSpout()) {
-            Bukkit.getServer().getPluginManager().registerEvents(new SkillSpoutListener(this), plugin);
+            Bukkit.getServer().getPluginManager().registerEvents(new SkillInvListener(), plugin);
         }else {
             Heroes.log(Level.WARNING, "SkillAlchemy requires Spout! Remove from your skills directory if you will not use!");
         }
     }
-    
+
     @Override
     public ConfigurationSection getDefaultConfig() {
         ConfigurationSection section = super.getDefaultConfig();
         section.set(Setting.LEVEL.node(), 1);
         return section;
     }
-    
-    public class SkillSpoutListener implements Listener {
-        
-        private final Skill skill;
-        public SkillSpoutListener(Skill skill) {
-            this.skill = skill;
-        }
-        
+
+    public class SkillInvListener implements Listener {
+
         @EventHandler(priority = EventPriority.LOW)
-        public void onInventoryOpen(InventoryOpenEvent event) {
-            if (event.isCancelled())
+        public void onPlayerInteract(PlayerInteractEvent event) {
+            if (event.isCancelled() || event.getAction() != Action.RIGHT_CLICK_BLOCK || event.getClickedBlock().getType() != Material.BREWING_STAND) {
                 return;
-            
+            }
             Hero hero = plugin.getHeroManager().getHero(event.getPlayer());
-            EntityPlayer eP = ((CraftPlayer) event.getPlayer()).getHandle();
-            if (!(eP.activeContainer instanceof ContainerBrewingStand))
-                return;
-            
-            if (!hero.canUseSkill(skill)) {
+            if (!hero.hasEffect(getName())) {
+                Messaging.send(event.getPlayer(), "You don't have the required skills to brew potions!");
                 event.setCancelled(true);
             }
         }
