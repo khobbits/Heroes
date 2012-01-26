7df27ce53b29397ae085632cf4c4d36de58bd8be
Sleaker <sleaker@comcast.net>
2012-01-26 09:09:15 -0800

    cancel a delayed skill whenever a hero is subject to a silence effect and the delayed skill is silencable

--- src/com/herocraftonline/dev/heroes/effects/common/SilenceEffect.java
+++ src/com/herocraftonline/dev/heroes/effects/common/SilenceEffect.java
@@ -3,7 +3,9 @@ package com.herocraftonline.dev.heroes.effects.common;
 import com.herocraftonline.dev.heroes.effects.EffectType;
 import com.herocraftonline.dev.heroes.effects.ExpirableEffect;
 import com.herocraftonline.dev.heroes.hero.Hero;
+import com.herocraftonline.dev.heroes.skill.DelayedSkill;
 import com.herocraftonline.dev.heroes.skill.Skill;
+import com.herocraftonline.dev.heroes.skill.SkillType;
 
 public class SilenceEffect extends ExpirableEffect {
 
@@ -16,6 +18,14 @@ public class SilenceEffect extends ExpirableEffect {
         types.add(EffectType.SILENCE);
     }
 
+    @Override
+    public void apply(Hero hero) {
+        super.apply(hero);
+        DelayedSkill dSkill = hero.getDelayedSkill();
+        if (dSkill != null && dSkill.getSkill().isType(SkillType.SILENCABLE)) {
+            hero.cancelDelayedSkill();
+        }
+    }
     @Override
     public void remove(Hero hero) {
         super.remove(hero);
